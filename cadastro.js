9ef27e94629f5f99821e0b710f86bc23f37e3b3e
Guilherme Tófoli <guitofoli1806@gmail.com>
2025-05-27 21:53:53 -0300

    Adicionado if para verificar se no campo CEP tem algum valor informado e caso não tenha será emitido um alerta

--- cadastro.js
+++ cadastro.js
@@ -52,6 +52,14 @@ const pesquisarCep = async() => {
 
     if(cepValido(cep.value)){
         const dados = await fetch(url);
+        const addres = await dados.json();
+
+        //hasOwnProperty retornará um valor booleano indicando se o objetivo possui a propriedades específicas no parenteses
+        if(addres.hasOwnProperty('erro')){
+            alert("CEP não encontrado");
+        } else{
+            preencherFormulario(addres);
+        }
     }
 }
 
